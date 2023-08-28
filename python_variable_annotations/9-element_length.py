a290c6b1dfb4fa4f4f1a416c4b2385b901ea11be
lenne sebastien <sebastienlenne@gmail.com>
2023-08-28 22:32:44 +0200

    add file for task 9

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+''' Module 8 - element_length '''
+import typing
+
+def element_length(lst: typing.Iterable[typing.Sequence]) -> typing.List[typing.Tuple[typing.Sequence, int]]:
+    return [(i, len(i)) for i in lst]
