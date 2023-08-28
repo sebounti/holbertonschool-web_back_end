361eef9eb31c526c5f9df80874afef88b658359e
lenne sebastien <sebastienlenne@gmail.com>
2023-08-28 22:34:40 +0200

    update file for task 9

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -3,5 +3,6 @@
 import typing
 
 
-def element_length(lst: typing.Iterable[typing.Sequence]) -> typing.List[typing.Tuple[typing.Sequence, int]]:
+def element_length(lst: typing.Iterable[typing.Sequence]) -> \
+    typing.List[typing.Tuple[typing.Sequence, int]]:
     return [(i, len(i)) for i in lst]
