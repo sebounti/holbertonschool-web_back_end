662e52a73abbfce1e915dde71888e56f5dc1922f
lenne sebastien <sebastienlenne@gmail.com>
2023-08-28 11:19:27 +0200

    add file for task 6

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+''' Module 6 - sum_mixed_list '''
+import math
+
+def sum_mixed_list(list_1: list, list_2: list) -> list:
+    ''' Function 5-sum_list '''
+    return [i + j for i, j in zip(list_1, list_2)]
