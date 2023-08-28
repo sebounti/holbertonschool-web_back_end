1106896441800e05e236d0ad273100261877c8f1
lenne sebastien <sebastienlenne@gmail.com>
2023-08-28 10:45:04 +0200

    add file for task 4

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''module 4-define_variables'''
+
+
+def define_variables():
+    '''module 4-define_variables'''
+    a = 1
+    pi = 3.14
+    i_understand_annotations = True
+    school = "Holberton"
+    return a, pi, i_understand_annotations, school
