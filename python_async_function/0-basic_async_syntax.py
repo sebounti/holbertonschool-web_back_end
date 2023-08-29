510dfafbe32cc83b22f0128d5d1b748825193991
lenne sebastien <sebastienlenne@gmail.com>
2023-08-29 10:23:48 +0200

    update file for task 0

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -5,6 +5,7 @@ import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
+    ''' Function 0 - wait_random '''
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
