85b39ac158cff6613082f060f79db76fe22107f7
lenne sebastien <sebastienlenne@gmail.com>
2023-08-29 08:33:29 +0200

    update file for task 4

--- python_variable_annotations/4-define_variables.py
+++ python_variable_annotations/4-define_variables.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
-'''module 4-define_variables'''
+# module = __import__('4-define_variables')
 
 
-def define_variables():
-    '''module 4-define_variables'''
-    a = 1
-    pi = 3.14
-    i_understand_annotations = True
-    school = "Holberton"
-    return a, pi, i_understand_annotations, school
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
