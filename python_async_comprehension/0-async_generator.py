f8ddbbc12cbd51cf424e688de06bb2ea321fc2aa
lenne sebastien <sebastienlenne@gmail.com>
2023-08-29 13:49:27 +0200

    add file for task 0

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+''' Module 0 - async_generator '''
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    ''' Function 0 - async_generator'''
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
