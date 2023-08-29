69d5feb729b4ff111f929dcb4a8ff3ad3a4ec8d5
lenne sebastien <sebastienlenne@gmail.com>
2023-08-29 08:37:13 +0200

    update file for task 6

--- python_variable_annotations/6-sum_mixed_list.py
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 ''' Module 6 - sum_mixed_list '''
-import math
+from typing import List, Union
 
 
-def sum_mixed_list(list_1: list, list_2: list) -> list:
-    ''' Function 5-sum_list '''
-    return [i + j for i, j in zip(list_1, list_2)]
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    ''' Function 5-sum_mixed_list '''
+    return [i + j for i, j in zip(mxd_lst)]
