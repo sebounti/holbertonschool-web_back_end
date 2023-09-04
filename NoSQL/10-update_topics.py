a174b4482c9d9891ba06534eb1127c8f6d2c1eed
lenne sebastien <sebastienlenne@gmail.com>
2023-09-04 23:29:37 +0200

    add file for task 10

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python3
-
+#!/usr/bin/env python3
 '''function that changes all topics of a school document based on the name'''
+
+import pymongo
 
 
 def update_topics(mongo_collection, name, topics):
