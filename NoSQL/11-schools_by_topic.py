157403ab1525ea7c0864b3ae1bc3787bc5c5dad4
lenne sebastien <sebastienlenne@gmail.com>
2023-09-05 09:21:37 +0200

    update file for task 11

--- NoSQL/11-schools_by_topic.py
+++ NoSQL/11-schools_by_topic.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-''' returns the list of school having a specific topic '''
+""" returns the list of school having a specific topic"""
 
 
 def schools_by_topic(mongo_collection, topic):
+    """ return a list"""
     return mongo_collection.find({'topics': topic})
