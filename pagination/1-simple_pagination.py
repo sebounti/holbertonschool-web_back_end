11a8c38e544ad40fba6c598de2bc7243ae366795
lenne sebastien <sebastienlenne@gmail.com>
2023-09-05 13:51:28 +0200

    update file from task 1

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -21,8 +21,7 @@ class Server:
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """Cached dataset"""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -33,7 +32,7 @@ class Server:
 
 
 def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-    '''  '''
+    ''' Function 1 - get_page '''
     assert type(page) == int
     assert type(page_size) == int
     assert page > 0
