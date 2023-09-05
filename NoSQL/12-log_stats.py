a63f5decb4abfebdf0334284556efa1c1594ad36
lenne sebastien <sebastienlenne@gmail.com>
2023-09-05 10:02:44 +0200

    add file for task 12

---
+++ NoSQL/12-log_stats.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""script that provides some stats about Nginx logs stored in MongoDB """
+
+import pymongo
+
+
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["logs"]
+collection = db["nginx"]
+
+total_logs = collection.count_documents({})
+
+methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+method_counts = {method: collection.count_documents({"method": method}) for method in methods}
+
+specific_criteria_count = collection.count_documents({"method": "GET", "path": "/status"})
+
+
+print(f"{total_logs} logs")
+print("Methods:")
+for method in methods:
+    print(f"   method {method}: {method_counts[method]}")
+print(f"{specific_criteria_count} status check")
