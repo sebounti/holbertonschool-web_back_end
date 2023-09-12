ae47832560a241aa6949adcbb35e3540831a65ae
lenne sebastien <sebastienlenne@gmail.com>
2023-09-12 14:38:33 +0200

    update file task 8

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -4,11 +4,16 @@ function getCurrentYear() {
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
+  // Creates a 'budget' object with keys calculated using the year
+  // actual year.
+  // The keys are created using string templates of
+  // characters (` `) to incorporate the current
+  // the actual year in the name of each key.
   const budget = {
 
   [`income-${getCurrentYear()}`]:income,
   [`gdp-${getCurrentYear()}`]: gdp,
   [`capita-${getCurrentYear()}`]: capita,
-  }
+  };
   return budget;
 }
