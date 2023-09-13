b6c7330f5acc33fb033f01c8c64fbc0c8e661eb9
lenne sebastien <sebastienlenne@gmail.com>
2023-09-13 17:04:01 +0200

    update file for task 10

---
+++ ES6_classes/10-car.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+/* 10-cars.js */
+
+class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+cloneCar() {
+  return new this.constructor(this._brand, this._motor, this._color);
+  }
+}
+
+export default Car;
