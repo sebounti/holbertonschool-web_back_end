e47af0e705ec7ad6354613c10363a299a8dd0667
lenne sebastien <sebastienlenne@gmail.com>
2023-09-13 10:31:25 +0200

    add file for task 4

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,34 @@
+/*  4-pricing.js */
+import currency from './3-currency.js';
+
+class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    this._amount = newAmount;
+  }
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    this._currency = newCurrency;
+  }
+
+  displayFullPrice() {
+    const { amount, currency } = this;
+    return `${amount} ${currency.name} (${currency.code})`;
+  }
+
+  static convertPrice(amount, conversionrate) {
+    return amount * conversionrate;
+  }
+}
+
+export default Pricing;
