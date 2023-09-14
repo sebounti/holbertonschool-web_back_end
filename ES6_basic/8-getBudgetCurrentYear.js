575b289ef4480e63237cad2275c2fba260c80b8c
lenne sebastien <sebastienlenne@gmail.com>
2023-09-14 10:34:28 +0200

    update  file for task 8

--- ES6_basic/8-getBudgetCurrentYear.js
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -1,21 +1,14 @@
-// Cette fonction retourne l'année actuelle.
 function getCurrentYear() {
   const date = new Date();
   return date.getFullYear();
 }
 
-// Cette fonction crée un objet 'budget' avec des clés calculées en utilisant
-// l'année actuelle.
-// Les clés sont créées en utilisant des templates de chaînes de caractères
-//(` `) pour incorporer
-// l'année actuelle dans le nom de chaque clé. Les valeurs 'income',
-//'gdp', et 'capita' sont
-// associées aux clés correspondantes.
+
 export default function getBudgetForCurrentYear(income, gdp, capita) {
   const budget = {
-    [`income-${getCurrentYear()}`]: income,
-    [`gdp-${getCurrentYear()}`]: gdp,
-    [`capita-${getCurrentYear()}`]: capita,
+  [`income-${getCurrentYear()}`]: income,
+  [`gdp-${getCurrentYear()}`]: gdp,
+  [`capita-${getCurrentYear()}`]: capita,
   };
 
   return budget;
