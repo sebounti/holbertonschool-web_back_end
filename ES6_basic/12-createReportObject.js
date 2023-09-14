55a3402a3e557b5205622be525fa3e507b093d05
lenne sebastien <sebastienlenne@gmail.com>
2023-09-14 10:51:45 +0200

    update  file for task 12

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,7 +1,7 @@
 export default function createReportObject(employeesList) {
   return {
     allEmployees: employeesList,
-    getNumberOfDepartments: function (employeesList) {
+    getNumberOfDepartments:(employeesList) => {
       return Object.keys(employeesList).length;
     },
   };
