aabfa67e740312b9d2d5f7a627a24c454a2a3676
lenne sebastien <sebastienlenne@gmail.com>
2023-10-02 22:30:54 +0200

    add  file 6-final-users.js task 0 for project 1 ES6 promise

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,13 +2,13 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, filename) {
-  return Promise.all([
+  return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(filename),
   ]).then((res) => {
     for (const x of res) {
       if (x.status === 'rejected') {
-        x.value = `error: ${x.raison.message}`;
+        x.value = `error: ${x.reason.message}`;
         delete x.raison;
       }
     }
