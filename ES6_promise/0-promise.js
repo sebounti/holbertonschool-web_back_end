7adaf12ce9022649c2f72d957e65694d63fefec4
lenne sebastien <sebastienlenne@gmail.com>
2023-10-02 13:52:15 +0200

    add file 0-promise.js task 0 for project 1 ES6 promise/ file readme

---
+++ ES6_promise/0-promise.js
@@ -0,0 +1,11 @@
+/* 0_promise.js */
+export default function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+    const condition = true;
+    if (condition) {
+      resolve(`true`);
+    } else {
+      reject(new Error("false error message"));
+    }
+  });
+}
