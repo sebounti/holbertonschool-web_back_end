6abd1eeb975e4eff3430983cabad52b79c81b01a
lenne sebastien <sebastienlenne@gmail.com>
2023-10-03 14:22:42 +0200

    update file error data  0-get_list_students.js, 1-get_list_student_ids for project 2 javascript date manipulation

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,18 +1,18 @@
 const getliststudents = () => [
   {
     id: 1,
-    firstName: "Guillaume",
-    location: "San Francisco",
+    firstName: 'Guillaume',
+    location: 'San Francisco',
   },
   {
     id: 2,
-    firstName: "James",
-    location: "San Francisco",
+    firstName: 'James',
+    location: 'San Francisco',
   },
   {
-    id: 3,
-    firstName: "Serena",
-    location: "San Francisco",
+    id: 5,
+    firstName: 'Serena',
+    location: 'San Francisco',
   },
 ];
 
