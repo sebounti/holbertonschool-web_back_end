41e96c4c8271ae7d2b35012fa59801699917f440
lenne sebastien <sebastienlenne@gmail.com>
2023-10-03 21:53:47 +0200

    update error bananas file 9-groceries for project 2 javascript data manipulation

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,11 +1,11 @@
 function groceriesList() {
   const groceries = new Map();
 
-  groceries.set('Apples', 10);
-  groceries.set('Tomatoes', 10);
-  groceries.set('Pasta', 1);
-  groceries.set('Rice', 1);
-  groceries.set('Bananas', 5);
+  groceries.set("Apples", 10);
+  groceries.set("Tomatoes", 10);
+  groceries.set("Pasta", 1);
+  groceries.set("Rice", 1);
+  groceries.set("Banana", 5);
 
   return groceries;
 }
