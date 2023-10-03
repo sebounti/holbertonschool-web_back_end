aa0a0478716a30673aceb7e07cb8a74367f010db
lenne sebastien <sebastienlenne@gmail.com>
2023-10-03 10:39:44 +0200

    update  file 6-final-user.js task 0 for project 1 ES6 promise

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,17 +1,19 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
 
-export default function handleProfileSignup(firstName, lastName, filename) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(filename),
-  ]).then((res) => {
-    for (const x of res) {
-      if (x.status === 'fulfilled') {
-        x.value = `error: ${x.reason.message}`;
-        delete x.reason;
-      }
-    }
-    return res;
-  });
-}
+const handleProfileSignup = async (firstName, lastName, fileName) => {
+  const result = [];
+  try {
+    const user = await signUpUser(firstName, lastName);
+    result.push({ status: "fulfilled", value: user });
+    await uploadPhoto(fileName);
+  } catch (error) {
+    result.push({
+      status: "rejected",
+      value: error.toString(),
+    });
+  }
+  return result;
+};
+
+export default handleProfileSignup;
