3206ea446b3c0a958d8b52ed547f1044f4f7f029
lenne sebastien <sebastienlenne@gmail.com>
2023-10-03 10:41:20 +0200

    update linter  file 6-final-user.js task 0 for project 1 ES6 promise

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,15 +1,15 @@
-import signUpUser from "./4-user-promise";
-import uploadPhoto from "./5-photo-reject";
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 const handleProfileSignup = async (firstName, lastName, fileName) => {
   const result = [];
   try {
     const user = await signUpUser(firstName, lastName);
-    result.push({ status: "fulfilled", value: user });
+    result.push({ status: 'fulfilled', value: user });
     await uploadPhoto(fileName);
   } catch (error) {
     result.push({
-      status: "rejected",
+      status: 'rejected',
       value: error.toString(),
     });
   }
