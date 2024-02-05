da3fdbc518b1a4fae307780118a7b1ed3b1e86af
lenne sebastien <sebastienlenne@gmail.com>
2024-02-05 21:56:56 +0100

    update file error pep8

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-from base_caching import BaseCaching
 ''' BasicCache class '''
+from base_caching import BaseCaching
+
 
 
 class BasicCache(BaseCaching):
     """ Defines a class BasicCache that inherits from BaseCaching
       and is a caching system """
+
     def put(self, key, item):
         ''' Add an item to the cache'''
         if key is not None and item is not None:
