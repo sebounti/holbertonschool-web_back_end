585b6458cebade0e28cec701fbb684504a987a10
lenne sebastien <sebastienlenne@gmail.com>
2024-02-05 22:06:24 +0100

    update file error pep8

--- caching/3-lru_cache.py
+++ caching/3-lru_cache.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-from base_caching import BaseCaching
 ''' LRU Caching System '''
+from base_caching import BaseCaching
+
 
 class LRUCache(BaseCaching):
     ''' LRU Caching System '''
@@ -14,12 +15,12 @@ class LRUCache(BaseCaching):
         ''' Add an item in the cache
         '''
         if key is not None and item is not None:
-                if key in self.cache_data:
-                    self.keys.remove(key)
-                elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                    del self.cache_data[self.keys[0]]
-                    print("DISCARD:", self.keys[0])
-                    self.keys.pop(0)
+            if key in self.cache_data:
+                self.keys.remove(key)
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                del self.cache_data[self.keys[0]]
+                print("DISCARD:", self.keys[0])
+                self.keys.pop(0)
                 self.keys.append(key)
                 self.cache_data[key] = item
 
