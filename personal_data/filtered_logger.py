41e58510cc311b06a17db0c2ffe9c2719bff31cd
lenne sebastien <sebastienlenne@gmail.com>
2024-02-19 10:06:00 +0100

    add all file for project personnal data task 0-Regex-ing'

---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+''' Filtered logger '''
+import re
+
+
+def filter_datum(fields, redaction, message, separator):
+    ''' Returns the log message obfuscated '''
+    return re.sub(r'({})(?={}|$)'.format('|'.join(fields), separator),
+                  redaction, message)
