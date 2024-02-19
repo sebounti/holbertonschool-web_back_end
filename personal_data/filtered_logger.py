5080c5eea31d7d554cf44cc1df40d7db42830a5f
lenne sebastien <sebastienlenne@gmail.com>
2024-02-19 10:16:49 +0100

    update file error annotated

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 ''' Filtered logger '''
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     ''' Returns the log message obfuscated '''
     return re.sub(r'({})(?={}|$)'.format('|'.join(fields), separator),
                   redaction, message)
