f235fac1f667d3bf3cb7db9c9e95a07e8fae7d1d
lenne sebastien <sebastienlenne@gmail.com>
2024-02-19 15:48:47 +0100

    add file encrypt_password - task 5

---
+++ personal_data/encrypt_password.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import bcrypt
+
+''' Task 5 - Encrypting passwords'''
+
+
+def hash_password(password: str) -> str:
+    ''' Returns a hashed password '''
+    return bcrypt.hashpw(password.encode('utf-8'),
+                         bcrypt.gensalt(prefix=b"2b"))
