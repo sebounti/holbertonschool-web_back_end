d31998125c97b228fc72970fa88868f1ad5d1158
lenne sebastien <sebastienlenne@gmail.com>
2024-02-19 15:08:25 +0100

    update file task 3-connect to secure databass

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,8 +3,26 @@
 import re
 from typing import List
 import logging
+import mysql.connector
+import os
 
 PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+'''Task 0 - Regex-ing'''
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    ''' Returns the log message obfuscated '''
+    for field in fields:
+        message = re.sub(f'{field}=.+?{separator}',
+                         f'{field}={redaction}{separator}', message)
+
+    return message
+
+
+'''Task 1 - Log formatter '''
 
 
 class RedactingFormatter(logging.Formatter):
@@ -27,14 +45,7 @@ class RedactingFormatter(logging.Formatter):
         return (super(RedactingFormatter, self).format(record))
 
 
-def filter_datum(fields: List[str], redaction: str, message: str,
-                 separator: str) -> str:
-    ''' Returns the log message obfuscated '''
-    for field in fields:
-        message = re.sub(f'{field}=.+?{separator}',
-                         f'{field}={redaction}{separator}', message)
-
-    return message
+'''Task 2 - Create logger '''
 
 
 def get_logger() -> logging.Logger:
@@ -47,3 +58,24 @@ def get_logger() -> logging.Logger:
     logger.addHandler(stream)
 
     return logger
+
+
+''' Task 3 - Connect to secure database '''
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    ''' Returns a connector to a database '''
+
+    username = os.getenv('PERSONAL_DATA_DB_USERNAME', 'root')
+    password = os.getenv('PERSONAL_DATA_DB_PASSWORD', '')
+    hosting = os.getenv('PERSONAL_DATA_DB_HOST', 'localhost')
+    db_name = os.getenv('PERSONAL_DATA_DB_NAME')
+
+    database = mysql.connector.connect(
+        host=hosting,
+        user=username,
+        password=password,
+        database=db_name
+    )
+
+    return database
