f6f3303b093a66163d5fa4315d2ce746511dd6ab
lenne sebastien <sebastienlenne@gmail.com>
2024-02-19 15:51:32 +0100

    update file encrypt_password - error annoated

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -5,7 +5,7 @@ import bcrypt
 ''' Task 5 - Encrypting passwords'''
 
 
-def hash_password(password: str) -> str:
+def hash_password(password: str= '') -> bytes:
     ''' Returns a hashed password '''
     return bcrypt.hashpw(password.encode('utf-8'),
                          bcrypt.gensalt(prefix=b"2b"))
