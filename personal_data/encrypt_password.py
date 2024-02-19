168a2934d4e603fcf77891f011fabdd06e57e380
lenne sebastien <sebastienlenne@gmail.com>
2024-02-19 16:08:05 +0100

    update file encrypt_password - task 6 - Check valid password

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -3,8 +3,18 @@
 
 import bcrypt
 
+'''task 5 - Encrypting passwords'''
+
 
 def hash_password(password: str = '') -> bytes:
     ''' Returns a hashed password '''
     return bcrypt.hashpw(password.encode('utf-8'),
                          bcrypt.gensalt(prefix=b"2b"))
+
+
+''' Task 6 - Check valid password '''
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    ''' Returns a hashed password '''
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
