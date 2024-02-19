696f58c054decb28569777fd8530c6a5d962fd04
lenne sebastien <sebastienlenne@gmail.com>
2024-02-19 10:44:18 +0100

    update file error code return

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 ''' Filtered logger '''
-import re
+from re import sub
 from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     ''' Returns the log message obfuscated '''
-    return re.sub(r'({})(?={}|$)'.format('|'.join(fields), separator),
-                  redaction, message)
+    for field in fields:
+        message = sub(f'{field}=.+?{separator}',
+                      f'{field}={redaction}{separator}', message)
+
+    return message
