6e8ea257698e44402f1084bb37901270252f54cf
lenne sebastien <sebastienlenne@gmail.com>
2024-02-20 10:58:51 +0100

    update file auth.py error pycodestyle

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -4,20 +4,24 @@
 from flask import request
 from typing import List, TypeVar
 
+
 class Auth:
-    """Auth class
+    """
+    Auth class
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
             Define which routes don't need authentication
         """
-        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+        if path is None or ex_path is None or len(ex_path) == 0:
             return True
-        if path[-1] != '/':
-            path += '/'
-        if path in excluded_paths:
-            return False
-        return True
+        for ex_path in excluded_paths:
+            if ex_path == path:
+                return False
+            if ex_path.endswith('*'):
+                pfx_path = ex_path[:-1]
+            if pfx_path in path:
+                return False
 
     def authorization_header(self, request=None) -> str:
         """
