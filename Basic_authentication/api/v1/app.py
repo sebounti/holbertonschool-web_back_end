d81c01e37e31c699be0cba3a34593f8728a0dece
lenne sebastien <sebastienlenne@gmail.com>
2024-02-20 15:55:45 +0100

    update file app.py

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -12,7 +12,14 @@ import os
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+auth = None
 
+auth_type = os.getenv("AUTH_TYPE")  # Utilisation de os.getenv directement
+
+if auth_type:
+    if auth_type == "custom_auth":
+        from api.v1.auth.auth import Auth
+        auth = Auth()
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -36,6 +43,22 @@ def forbidden(error) -> str:
     """
     return jsonify({"error": "Forbidden"}), 403
 
+
+@app.before_request
+def handle_before_request():
+    """ Request validation """
+    if auth is None:
+        return
+    excluded_paths = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/']
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
