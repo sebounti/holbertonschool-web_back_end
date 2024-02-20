fed12a72095700a592f94e8a7174946a641626bd
lenne sebastien <sebastienlenne@gmail.com>
2024-02-20 14:10:03 +0100

    update file app.py

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -19,20 +19,24 @@ if auth_type:
         from api.v1.auth.auth import Auth
         auth = Auth()
 
+
 @app.errorhandler(404)
-def not_found(error):
+def not_found(error)-> str:
     """ Not found handler """
     return jsonify({"error": "Not found"}), 404
 
+
 @app.errorhandler(401)
-def Unauthorized(error):
+def Unauthorized(error) -> str:
     """ Unauthorized handler """
     return jsonify({"error": "Unauthorized"}), 401
 
+
 @app.errorhandler(403)
-def Forbidden(error):
+def Forbidden(error)-> str:
     """ Forbidden Handler """
     return jsonify({"error": "Forbidden"}), 403
+
 
 @app.before_request
 def handle_before_request():
@@ -49,6 +53,7 @@ def handle_before_request():
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
+
 
 if __name__ == "__main__":
     host = os.getenv("API_HOST", "0.0.0.0")
