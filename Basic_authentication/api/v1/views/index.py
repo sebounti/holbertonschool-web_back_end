c10d6bb73924d97790143a9f945296a234b24e44
lenne sebastien <sebastienlenne@gmail.com>
2024-02-20 13:48:19 +0100

    update file index.py

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -26,7 +26,7 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized', strict_slashes=False)
+@app_views.route('/unauthorized',methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     """ Get /api/v1/unauthorized
     Return:
@@ -35,7 +35,7 @@ def unauthorized() -> str:
     return abort(401)
 
 
-@app_views.route('/Forbidden', strict_slashes=False)
+@app_views.route('/Forbidden', methods=['GET'], strict_slashes=False)
 def Forbidden() -> str:
     """ Get /api/v1/Forbidden
     Return:
