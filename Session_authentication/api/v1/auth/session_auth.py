ffa8052934be4e6fe010597114c12474c0f469a5
lenne sebastien <sebastienlenne@gmail.com>
2024-02-21 14:13:10 +0100

    update file session_auth for task 3 - User ID for Session ID

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -27,3 +27,13 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[session_id] = user_id
 
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+
+        ''' Return a User ID based on a Session ID'''
+        if session_id is None or not isinstance(session_id, str):
+            return None
+
+        user_id = self.user_id_by_session_id.get(session_id)
+
+        return user_id
