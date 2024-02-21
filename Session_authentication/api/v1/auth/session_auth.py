3986116786f9e3c76d05153a884dd1d52c362dfb
lenne sebastien <sebastienlenne@gmail.com>
2024-02-21 13:57:42 +0100

    update file session_auth for task 2 - Create a session

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -2,12 +2,26 @@
 """ Module of Index views
 """
 
-from api.v1.auth.auth import Auth
+from api.v1.views import app_views
+from flask import abort, jsonify, request
 from models.user import User
+from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
     '''
         Session Authentication
     '''
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = super().create_session(user_id)
+        if session_id is None:
+            return None
+
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
