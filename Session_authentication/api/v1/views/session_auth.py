fa54fa732f93fb8468e8274946ca4adf08bb3e1f
lenne sebastien <sebastienlenne@gmail.com>
2024-02-21 16:32:14 +0100

    update all file sessiom_auth

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -5,7 +5,7 @@ from api.v1.views import app_views
 from flask import abort, jsonify, request
 from models.user import User
 import os
-from api.v1.app import auth
+from api.v1.app import Auth
 
 
 @app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
@@ -43,7 +43,7 @@ def login():
 def logout():
     ''' logout session '''
     # Détruire la session de l'utilisateur
-    if not auth.destroy_session(request):
+    if not Auth.destroy_session(request):
         # Si la session n'a pas pu être détruite, retourner une erreur 404
         abort(404)
 
