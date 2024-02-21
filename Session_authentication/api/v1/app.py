775496a18f37cbe98cfb7ee1a0e9f0a23d3b8444
lenne sebastien <sebastienlenne@gmail.com>
2024-02-21 13:49:05 +0100

    update file for task 1 - Empty session

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -8,7 +8,6 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
 
-
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
@@ -20,6 +19,9 @@ if AUTH_TYPE == "basic_auth":
 elif AUTH_TYPE == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
+elif getenv('AUTH_TYPE') == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 
 
 @app.before_request
---
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/ env python3
+""" Module of Index views
+"""
+
+from api.v1.auth.auth import Auth
+from models.user import User
+
+
+class SessionAuth(Auth):
+    '''
+        Session Authentication
+    '''
+    pass
