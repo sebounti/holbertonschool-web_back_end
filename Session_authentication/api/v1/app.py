709ace5c2f62fddc5fb187af077bcce842c3660a
lenne sebastien <sebastienlenne@gmail.com>
2024-02-21 14:38:28 +0100

    update file app.py for task 5-Before request

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -35,20 +35,14 @@ def handle_before_request():
         '/api/v1/forbidden/',
         '/api/v1/auth_session/login']
 
-    if not (auth.require_auth(request.path, excluded_paths)):
-        return
+    if auth.require_auth(request.path, excluded_paths):
+            if (auth.authorization_header(request) is None and
+                    auth.session_cookie(request) is None):
+                abort(401)
+            if auth.current_user(request) is None:
+                abort(403)
+            request.current_user = auth.current_user(request)
 
-    if (auth.authorization_header(request)) is None:
-        abort(401)
-
-    if auth.current_user(request) is None:
-        abort(403)
-
-    if (auth.authorization_header(request) is None and
-                auth.session_cookie(request) is None):
-        abort(401)
-
-    request.current_user = auth.current_user(request)
 
 
 @app.errorhandler(404)
