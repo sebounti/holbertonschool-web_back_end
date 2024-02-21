d1be1f3262980dc27cffe1927fd54d42f6c9ef9e
lenne sebastien <sebastienlenne@gmail.com>
2024-02-21 11:29:43 +0100

    update file app.py for task 0 - Et moi et moi et moi

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -38,7 +38,8 @@ def handle_before_request():
     if (auth.authorization_header(request)) is None:
         abort(401)
 
-    if (auth.current_user(request)) is None:
+    current_user = auth.current_user(request)
+    if current_user is None:
         abort(403)
 
     request.current_user = current_user
