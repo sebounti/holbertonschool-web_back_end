fa5450aae1b6468212f06f32c5aca16ab366875c
lenne sebastien <sebastienlenne@gmail.com>
2024-02-21 10:45:13 +0100

    update files from task 10-Basic - User object

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,9 +2,9 @@
 """ Basic authentication module"""
 
 from api.v1.auth.auth import Auth
-from typing import List, TypeVar
+from typing import TypeVar
 from base64 import b64decode
-from flask import request
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -74,21 +74,19 @@ class BasicAuth(Auth):
     def user_object_from_credentials(self, user_email: str, user_pwd: str
                                      ) -> TypeVar('User'):
 
-            """
-            User object from credentials
-            """
+        """ User object from credentials """
 
-            if user_email is None or not isinstance(user_email, str):
-                return None
+        if user_email is None or not isinstance(user_email, str):
+            return None
 
-            if user_pwd is None or not isinstance(user_pwd, str):
-                return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
 
-            try:
-                found_users = User.search({'email': user_email})
-            except Exception:
-                return None
+        try:
+            found_users = User.search({'email': user_email})
+        except Exception:
+            return None
 
-            for user in found_users:
-                if user.is_valid_password(user_pwd):
-                    return user
+        for user in found_users:
+            if user.is_valid_password(user_pwd):
+                return user
