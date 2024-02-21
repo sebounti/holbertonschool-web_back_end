1ecd980225095acafb7ba6d50e260cffc0145c5a
lenne sebastien <sebastienlenne@gmail.com>
2024-02-21 14:36:38 +0100

    update file app.py for task 5-Before request

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -44,9 +44,9 @@ def handle_before_request():
     if auth.current_user(request) is None:
         abort(403)
 
-    if auth.authorization_header(request
-                                 ) and auth.session_cookie(request) is None:
-        abort(403)
+    if (auth.authorization_header(request) is None and
+                auth.session_cookie(request) is None):
+        abort(401)
 
     request.current_user = auth.current_user(request)
 
