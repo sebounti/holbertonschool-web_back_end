edeb78bda290c9861b81f163bce6e9adde27f401
lenne sebastien <sebastienlenne@gmail.com>
2024-02-26 16:12:59 +0100

    update file db.py task 10 Get session ID

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -7,7 +7,7 @@ from db import DB
 from user import User, Base
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
-import uuid
+from uuid import uuid4
 
 
 def _hash_password(password: str) -> bytes:
@@ -19,7 +19,7 @@ def _hash_password(password: str) -> bytes:
 def _generate_uuid() -> str:
     """Generate a UUID
     """
-    return str(uuid.uuid4())
+    return str(uuid4())
 
 
 class Auth:
