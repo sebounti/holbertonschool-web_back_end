a2c302ca07f923defe74a9fb6f7ca61b438ad30e
lenne sebastien <sebastienlenne@gmail.com>
2024-02-26 23:38:16 +0100

    update file app.py task 16 - Generate reset password token

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -114,25 +114,22 @@ class Auth:
         '''
         self._db.update_user(user_id, session_id=None)
 
-    def get_user_from_session_id(self, email: str) -> str:
+    def get_reset_password_token(self, email: str) -> str:
         '''
-        Generate reset password token
+        Get reset password token
 
         Args:
-            email: reset password token
+            email: email of the user
 
         Return:
-            generate token
+            reset token
         '''
-        if email is None:
-            raise ValueError
-
         try:
-            user = self._db.find_user_by(session_id=email)
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
 
-            token: str = _generate_uuid()
-            self._db.update_user(user.id, reset_token=token)
+            return reset_token
 
-            return token
-        except (NoResultFound, InvalidRequestError):
+        except NoResultFound:
             raise ValueError
