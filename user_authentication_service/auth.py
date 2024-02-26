82433e7a83da9c47d06466bf6eb6d0625c659eb3
lenne sebastien <sebastienlenne@gmail.com>
2024-02-26 15:53:29 +0100

    update file db.py task 9 Generate UUIDs

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -7,12 +7,19 @@ from db import DB
 from user import User, Base
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
+import uuid
 
 
 def _hash_password(password: str) -> bytes:
     """Encrypting passwords
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+def _generate_uuid() -> str:
+    """Generate a UUID
+    """
+    return str(uuid.uuid4())
 
 
 class Auth:
