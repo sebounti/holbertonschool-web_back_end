b12af8fa2c765c891e76502e3b78104d3753fdd4
lenne sebastien <sebastienlenne@gmail.com>
2024-02-26 15:26:58 +0100

    update file db.py task 5 Register user

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -3,9 +3,40 @@
 encrypting passwords
 """
 import bcrypt
+from db import DB
+from user import User, Base
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 
 def _hash_password(password: str) -> bytes:
     """Encrypting passwords
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Register a new user with the database.
+
+        Args:
+            email: user email.
+            password: user password.
+
+        Returns:
+            User: The user object.
+        """
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError("User {} already exists".format(email))
+
+        except NoResultFound:
+            passwd: str = _hash_password(password)
+            user = self._db.add_user(email, passwd)
