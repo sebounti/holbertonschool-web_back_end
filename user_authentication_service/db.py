8811e92d0e8a83f514ea1ec07f03e8d13cf6b224
lenne sebastien <sebastienlenne@gmail.com>
2024-02-26 09:31:26 +0100

    update file db.py error return code

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -31,7 +31,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password) -> User:
+    def add_user(self, email: str, hashed_password: str) -> User:
         """Add a new user to the database
 
         Args:
