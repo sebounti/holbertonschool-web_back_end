8a0961f63cc7ebd3c326fc81655d7ef53e47fa77
lenne sebastien <sebastienlenne@gmail.com>
2024-02-26 16:02:34 +0100

    update file db.py task 10 Get session ID

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -65,3 +65,22 @@ class Auth:
                                   user.hashed_password)
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        '''
+        Make session id and update in the database
+
+        Args:
+            email: email of the user
+
+        Return:
+            session id
+        '''
+
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
