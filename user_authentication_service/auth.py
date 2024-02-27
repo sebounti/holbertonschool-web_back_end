6f36423fa38b3c9fa936668b9d95b113cf638a92
lenne sebastien <sebastienlenne@gmail.com>
2024-02-27 14:32:48 +0100

    update file app.py task 18

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -102,14 +102,24 @@ class Auth:
             return None
 
     def destroy_session(self, user_id: int) -> None:
-        '''
-        Destroy session id
+        """
+        Destroy session
 
         Args:
-            user_id: user id
-        '''
-        self._db.update_user(user_id, session_id=None)
 
+            user_id: user id
+
+        Returns:
+
+            None
+
+
+        """
+        try:
+            user = self._db.find_user_by(id=user_id)
+            return setattr(user, 'session_id', None)
+        except Exception:
+            return None
 
     def get_reset_password_token(self, email: str) -> str:
         """Generate and reset password token"""
