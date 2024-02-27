f70d6344e7a0af2b9c2296c0e2980c751611246e
lenne sebastien <sebastienlenne@gmail.com>
2024-02-27 08:18:07 +0100

    update file app.py task 17 - Get reset password token

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -94,5 +94,22 @@ def profile() -> str:
     return jsonify({"email": user.email}), 200
 
 
+@ app.route('/reset_password', methods=['GET'])
+def get_reset_password_token() -> str:
+    '''
+    reset password
+    '''
+    email = request.args.get('email')
+
+    if email is None:
+        abort(403)
+
+    try:
+        token = AUTH.get_reset_password_token(email)
+        return jsonify({"email": email, "reset_token": token}), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
