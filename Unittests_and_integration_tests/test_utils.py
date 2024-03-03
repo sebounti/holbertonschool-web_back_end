ddbe361a7f66017a9b3be97b49b862f40b605412
lenne sebastien <sebastienlenne@gmail.com>
2024-03-04 00:55:37 +0100

    update file test_utils,py

---
+++ Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""
+file unittests for utils.py
+"""
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """ Access nested map """
+
+    @parameterized.expand([
+        ({'a': 1}, ['a'], 1),
+        ({'a': {'b': 2}}, ['a', 'b'], 2),
+        ({'a': {'b': 2}}, ['a'], {'b': 2}),
+    ])
+    def test_access_nested_map(self, nested_map, path, expected):
+        """
+        test access nested map
+
+        Args:
+        nested_map: dict
+            nested map
+        path: list
+            list of keys
+        result_expected: any
+            expected value
+
+        Returns:
+            ok if test passes
+        """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
