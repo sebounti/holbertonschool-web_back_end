3691c2349730a2bdfb9b2e35c7667b1dee49b8a3
lenne sebastien <sebastienlenne@gmail.com>
2024-03-03 10:27:25 +0100

    add file auth.py from project update error

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -8,11 +8,14 @@ import uuid
 from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
+
 
 def _hash_password(password: str) -> bytes:
     """Encrypting passwords
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
 
 def _generate_uuid() -> str:
     """Generate a UUID
@@ -70,45 +73,56 @@ class Auth:
             user = self._db.find_user_by(email=email)
             session_id = _generate_uuid()
             self._db.update_user(user.id, session_id=session_id)
+            return session_id
+
         except NoResultFound:
             return None
-
-        return session_id
-
 
     def get_user_from_session_id(self, session_id: str) -> str:
         """ Get user from session id """
         if session_id is None:
             return None
-
         try:
             user = self._db.find_user_by(session_id=session_id)
             return user
         except NoResultFound:
             return None
 
+    def destroy_session(self, user_id: int) -> None:
+        """ Destroys a session """
+        try:
+            self._db.update_user(user_id, session_id=None)
+            return None
+
+        except Exception:
+            return None
+
     def get_reset_password_token(self, email: str) -> str:
         """ Generate and reset password token """
+        if email is None:
+            return None
+
         try:
             user = self._db.find_user_by(email=email)
-            user.reset_token = _generate_uuid()
-            self._db._session.add(user)
-            self._db._session.commit()
-            return user.reset_token
-        except Exception:
+            token: str = _generate_uuid()
+            self._db.update_user(user.id, reset_token=token)
+            return token
+
+        except (NoResultFound, InvalidRequestError):
             raise ValueError
 
     def update_password(self, reset_token: str, password: str) -> None:
         """ Method that Updates password """
+        if reset_token is None or password is None:
+            return None
+
         try:
             user = self._db.find_user_by(reset_token=reset_token)
-            if user:
-                password = _hash_password(password)
-                self._db.update_user(user.id, hashed_password=password,
-                                     reset_token=None)
-        except NoResultFound:
+        except (NoResultFound, InvalidRequestError) as e:
             raise ValueError
 
-    def destroy_session(self, user_id: int) -> None:
-        """ Destroys a session """
-        self._db.update_user(user_id, session_id=None)
+        n_passwd: str = _hash_password(password)
+        self._db.update_user(user.id, hashed_password=n_passwd,
+                             reset_token=None)
+
+        return None
