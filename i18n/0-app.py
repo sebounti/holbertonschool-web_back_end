6cf52d0a92e0bfdc967c8cfec34eb95c082b37af
lenne sebastien <sebastienlenne@gmail.com>
2024-03-04 18:22:45 +0100

    update file 0-app.py 1-app.py

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,20 +1,8 @@
 #!/usr/bin/python3
 """ Module to start a Flask web application """
 from flask import Flask, render_template
-from flask_babel import Babel
 
 app = Flask(__name__)
-babel = Babel(app)
-
-
-class Config(object):
-    """ Configuration for languages """
-    LANGUAGES = ["en", "fr"]
-
-
-app.config.from_object(Config)
-Babel.default_locale = "en"
-Babel.default_timezone = "UTC"
 
 
 @app.route("/", methods=['GET'])
