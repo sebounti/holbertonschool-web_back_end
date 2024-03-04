cd82e003edc4dabdd2753c7d4d4d850fb5a5980c
lenne sebastien <sebastienlenne@gmail.com>
2024-03-04 09:55:25 +0100

    update file test_utils,py

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -32,5 +32,6 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
+
 if __name__ == "__main__":
     unittest.main()
