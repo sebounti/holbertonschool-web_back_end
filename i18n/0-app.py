5aba03706f30b8287b3398bc96e66287790acf89
lenne sebastien <sebastienlenne@gmail.com>
2024-03-04 17:50:16 +0100

    add folder project i18n and task 0

---
+++ i18n/0-app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Module to start a Flask web application """
+
+from flask import Flask, render_template
+from flask import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+@app.route('/')
+def hello_world():
+    """  Function to handle requests to the root of the web app"""
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
