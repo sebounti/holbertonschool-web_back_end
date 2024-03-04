5b55781bd88c3597883b6d5cc2df0860e33e8905
lenne sebastien <sebastienlenne@gmail.com>
2024-03-04 18:21:00 +0100

    update file 0-app.py task 1

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,14 +1,26 @@
 #!/usr/bin/python3
 """ Module to start a Flask web application """
 from flask import Flask, render_template
+from flask_babel import Babel
 
 app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """ Configuration for languages """
+    LANGUAGES = ["en", "fr"]
+
+
+app.config.from_object(Config)
+Babel.default_locale = "en"
+Babel.default_timezone = "UTC"
 
 
 @app.route("/", methods=['GET'])
 def index():
     """ route to display a message"""
-    return render_template("0-index.html")
+    return render_template("1-index.html")
 
 
 if __name__ == "__main__":
