662cbab33272d466740a6a34d1c1be958d642df0
lenne sebastien <sebastienlenne@gmail.com>
2024-03-04 11:06:45 +0100

    update file test_utils,py task 2

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -5,7 +5,7 @@ file unittests
 import unittest
 from parameterized import parameterized
 from unittest.mock import patch, Mock
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 from typing import Any, Dict, List, Tuple
 
 
@@ -30,6 +30,30 @@ class TestAccessNestedMap(unittest.TestCase):
         ''' test exception'''
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    ''' get json unittest '''
+    #  teste la fonction get_json avec différents paramètres.
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    def test_get_json(self, test_url, test_payload):
+        ''' self descriptive'''
+        # Classe interne Mocked pour simuler la réponse d'une requête HTTP.
+        class Mocked(Mock):
+            ''' mocked class'''
+
+            def json(self):
+                ''' json method mocked'''
+                return test_payload
+
+        # Utilise patch pour remplacer l'appel à requests.get par un objet mock.
+        with patch('requests.get') as MockClass:
+            MockClass.return_value = Mocked()
+            self.assertEqual(get_json(test_url), test_payload)
+
 
 
 if __name__ == "__main__":
