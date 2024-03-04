db710a166c061fa56634aeba34332d1b79ce72eb
lenne sebastien <sebastienlenne@gmail.com>
2024-03-04 21:07:45 +0100

    update file 0-app.py

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,11 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """ Module to start a Flask web application """
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route("/" , methods=['GET'])
+@app.route("/")
 def index():
     """ route to display a message"""
     return render_template("0-index.html")
