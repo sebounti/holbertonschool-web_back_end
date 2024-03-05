4a9b2bd2cab47ea674180635c4c7e4bd77edb046
lenne sebastien <sebastienlenne@gmail.com>
2024-03-05 11:04:10 +0100

    update file task 4 modification reauierement

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -1,41 +1,39 @@
 #!/usr/bin/env python3
-""" Module to start a Flask web application """
+"""Module to start a Flask web application."""
 from flask import Flask, render_template, request
 from flask_babel import Babel, _
 
-
 class Config(object):
-    """ Configuration for languages """
+    """Configuration for languages."""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
-
 
 app = Flask(__name__, template_folder='templates')
 app.config.from_object(Config)
 babel = Babel(app)
 
-
 @babel.localeselector
-def get_locale():
-    """ Locale language
+def get_locale() -> str:
+    """Locale language.
 
-        Return:
-            Best match to the language
+    Return:
+        Best match to the language.
     """
     user_locale = request.args.get('locale')
     if user_locale in app.config['LANGUAGES']:
         return user_locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
-
 @app.route('/', methods=["GET"])
-def hello_world():
+def hello_world() -> str:
     """
-    index template
+    Greeting.
+
+    Return:
+        Initial template html.
     """
     return render_template('4-index.html')
 
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port=5000)
