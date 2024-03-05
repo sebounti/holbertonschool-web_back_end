5d10666031ecf8293cfaca0248b4bdb9b62a666b
lenne sebastien <sebastienlenne@gmail.com>
2024-03-06 00:01:01 +0100

    modification file 5-app.py  for task 5

--- i18n/5-app.py
+++ i18n/5-app.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 """Module to start a Flask web application."""
 from flask import Flask, render_template, request, g
-
 from flask_babel import Babel
 
 
 app = Flask(__name__)
 babel = Babel(app)
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
 
 
 class Config:
@@ -18,13 +25,6 @@ class Config:
 
 app.config.from_object(Config)
 
-users = {
-    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
-    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
-    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
-    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
-}
-
 
 @app.route('/', methods=["GET"])
 def hello_world() -> str:
@@ -35,19 +35,6 @@ def hello_world() -> str:
         Initial template html.
     """
     return render_template('5-index.html', user=g.user)
-
-
-@babel.localeselector
-def get_locale() -> str:
-    """Locale language.
-
-    Return:
-        Best match to the language.
-    """
-    locale = request.args.get('locale')
-    if locale in Config.LANGUAGES:
-        return locale
-    return request.accept_languages.best_match(Config.LANGUAGES)
 
 
 def get_user(user_id) -> dict:
@@ -63,12 +50,26 @@ def get_user(user_id) -> dict:
         pass
     return None
 
+
 @app.before_request
 def before_request():
     """
-    Get user, if any
+    Get user, if any and set it as a global on flask.g.user
     """
     g.locale = str(get_locale())
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Locale language.
+
+    Return:
+        Best match to the language.
+    """
+    locale = request.args.get('locale')
+    if locale in Config.LANGUAGES:
+        return locale
+    return request.accept_languages.best_match(Config.LANGUAGES)
 
 
 if __name__ == "__main__":
