2018c7c1e71c8d09632c1fc09ad7b6effab0ea2b
lenne sebastien <sebastienlenne@gmail.com>
2024-03-05 21:45:25 +0100

    modification file test_utils.py for task 3

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -2,6 +2,7 @@
 """
 file unittests
 """
+import requests
 import unittest
 from parameterized import parameterized
 from unittest.mock import patch, Mock
@@ -16,8 +17,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
-    def test_access_nested_map(self, nested_map: Dict[Any, Any],
-                               path: List[str], expected: Any) -> None:
+    def test_access_nested_map(self, nested_map, path, expected):
         """ Test access nested map"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
@@ -25,8 +25,7 @@ class TestAccessNestedMap(unittest.TestCase):
         ({}, ("a",)),
         ({"a": 1}, ("a", "b"))
     ])
-    def test_access_nested_map_exception(self, nested_map: Dict[Any, Any],
-                                         path: List[str]):
+    def test_access_nested_map_exception(self, nested_map, path):
         ''' test exception'''
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
