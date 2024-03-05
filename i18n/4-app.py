55c1544051e6d7e667fd97b00ad4b12cdee504b0
lenne sebastien <sebastienlenne@gmail.com>
2024-03-05 11:06:12 +0100

    update file task 4 modification reauierement

--- i18n/4-app.py
+++ i18n/4-app.py
@@ -3,15 +3,18 @@
 from flask import Flask, render_template, request
 from flask_babel import Babel, _
 
+
 class Config(object):
     """Configuration for languages."""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
+
 app = Flask(__name__, template_folder='templates')
 app.config.from_object(Config)
 babel = Babel(app)
+
 
 @babel.localeselector
 def get_locale() -> str:
@@ -25,6 +28,7 @@ def get_locale() -> str:
         return user_locale
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
+
 @app.route('/', methods=["GET"])
 def hello_world() -> str:
     """
@@ -34,6 +38,7 @@ def hello_world() -> str:
         Initial template html.
     """
     return render_template('4-index.html')
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
