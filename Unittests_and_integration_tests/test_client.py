db06ab56e2f49a66a890694d168e0622e681fc28
lenne sebastien <sebastienlenne@gmail.com>
2024-03-05 22:53:47 +0100

    modification file test_client.py for task 5

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -59,3 +59,44 @@ class TestGithubOrgClient(unittest.TestCase):
     def test_has_license(self, repo, license_key, expected):
         client = GithubOrgClient("openai")
         self.assertEqual(client.has_license(repo, license_key), expected)
+
+@parameterized_class(('org_payload', 'repos_payload', 'expected_repos',
+                        'apache2_repos'),
+                        TEST_PAYLOAD)
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.get_patcher = patch('client.get_json')
+        cls.mock_get = cls.get_patcher.start()
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.get_patcher.stop()
+
+    def setUp(self):
+        self.get_patcher = patch('client.get_json')
+        self.mock_get = self.get_patcher.start()
+
+    def tearDown(self):
+        self.get_patcher.stop()
+
+    def test_public_repos(self):
+        self.mock_get.side_effect = [self.org_payload, self.repos_payload]
+        test_class = GithubOrgClient('test')
+        self.assertEqual(test_class.org, self.org_payload)
+        self.assertEqual(test_class.repos_payload, self.repos_payload)
+        self.assertEqual(test_class.public_repos(), self.expected_repos)
+        self.mock_get.assert_called_with(test_class._public_repos_url)
+
+    def test_public_repos_with_license(self):
+        self.mock_get.side_effect = [self.org_payload, self.repos_payload]
+        test_class = GithubOrgClient('test')
+        self.assertEqual(test_class.org, self.org_payload)
+        self.assertEqual(test_class.repos_payload, self.repos_payload)
+        self.assertEqual(test_class.public_repos('apache-2.0'), self.apache2_repos)
+        self.mock_get.assert_called_with(test_class._public_repos_url)
+
+    @classmethod
+    def tearDownClass(cls):
+        cls.get_patcher.stop()
