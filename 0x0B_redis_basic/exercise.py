9b7cc5126cdbedeab0f6f2eea3f326aab368e16d
lenne sebastien <sebastienlenne@gmail.com>
2024-03-18 15:12:25 +0100

    add update file exercise.py for task 3

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -18,6 +18,20 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    ''' Store the history of inputs and outputs for a particular function '''
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        input = str(args)
+        self._redis.rpush(method.__qualname__ + ":inputs", input)
+
+        output = str(method(self, *args, **kwargs))
+        self._redis.rpush(method.__qualname__ + ":outputs", output)
+        return output
+
+    return wrapper
+
+
 class Cache:
     ''' Cache class to store and retrieve data from Redis'''
     def __init__(self):
@@ -25,6 +39,7 @@ class Cache:
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         ''' Store data in Redis and return a unique key'''
         unique_key = str(uuid.uuid4())
