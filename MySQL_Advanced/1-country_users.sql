cd850e9352d0ed119cd2dbe79a3dfa10e55e89a3
lenne sebastien <sebastienlenne@gmail.com>
2024-03-19 09:56:59 +0100

    add task 1-country_users.sql

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,7 @@
 -- Make a table
 -- If table exist, you should not fail
 CREATE TABLE IF NOT EXISTS users (
-	id INT NOT NULL AUTO_ECREMENT,
+	id INT NOT NULL AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255),
 	country ENUM('US', 'CO','TN') NOT NULL DEFAULT 'US',
