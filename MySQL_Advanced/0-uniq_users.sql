54dc3782a196eb90f256100417f8b143932b269f
lenne sebastien <sebastienlenne@gmail.com>
2024-03-19 09:45:15 +0100

    update 0-uniq_users.sql add comment

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,7 +1,8 @@
 -- Make a table
 -- If table exist, you should not fail
 CREATE TABLE IF NOT EXISTS users (
-	id INT NOT NULL AUTO_INCREMENT,PRIMARY KEY,
+	id INT NOT NULL AUTO_INCREMENT,
 	email VARCHAR(255) NOT NULL UNIQUE,
 	name VARCHAR(255),
+	PRIMARY KEY (id)
 );
