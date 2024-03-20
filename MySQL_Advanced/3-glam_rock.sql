1652f5a8d7edcf2f1cc008d2f8ff4c8e61fbb2e4
lenne sebastien <sebastienlenne@gmail.com>
2024-03-20 20:48:42 +0100

    update file task 3-glam_rock.sql

---
+++ MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,6 @@
+-- Search bands with style Glam rock
+-- Durantion current band
+SELECT band_name AS band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
+ORDER BY lifespan DESC;
