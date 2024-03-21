44a3b0ca1d74b86f8a4e40a3955e8f0fa5b64d99
lenne sebastien <sebastienlenne@gmail.com>
2024-03-21 09:46:33 +0100

    update file task 10-div.sql

--- MySQL_Advanced/10-div.sql
+++ MySQL_Advanced/10-div.sql
@@ -1,9 +1,9 @@
--- 10. Safe divide
+-- path: 10-div.sql
 -- creates a function SafeDiv that divides (and returns) the first by the second number or returns 0 if the second number is equal to 0
 DROP FUNCTION IF EXISTS SafeDiv;
 DELIMITER $$
-CREATE FUNCTION SafeDiv(a INT, b INT) RETURNS DECIMAL (10, 2)
-DETERMINISTIC
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT DETERMINISTIC
 BEGIN
     IF (b = 0)
     THEN
