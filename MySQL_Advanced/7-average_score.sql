3ae6d495e2f8b671b623286b05aa329ada06e847
lenne sebastien <sebastienlenne@gmail.com>
2024-03-21 10:50:27 +0100

    update  task 7-average_score.sql

--- MySQL_Advanced/7-average_score.sql
+++ MySQL_Advanced/7-average_score.sql
@@ -1,13 +1,13 @@
 -- path 7-average_score.sql
 -- Description: Create a view that lists all students that have a score under 80 (strict)
-DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 CREATE PROCEDURE ComputeAverageScoreForUser(
 	IN student_id INT
 )
 BEGIN
 	DECLARE avg_score FLOAT;
-	SET avg_score = (SELECT AVG(score) FROM corrections AS C WHERE C.user_id=student_id);
-	UPDATE users SET average_score = avg_score WHERE id = user_id;
+    SELECT AVG(score) INTO avg_score FROM corrections WHERE user_id = student_id;
+	UPDATE users SET average_score = avg_score WHERE id = student_id;
 END$$
 DELIMITER ;
