20f0a4cdf27fcbaf5b526d8a761470f454767b07
lenne sebastien <sebastienlenne@gmail.com>
2024-03-21 11:57:24 +0100

    update task 2-fans.sql

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,5 +1,6 @@
+--path 2-fans.sql
 -- make a best band fans
-SELECT origin AS origin ,SUM(fans) AS nb_Fans
+SELECT origin AS origin , SUM(fans) AS nb_Fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_Fans DESC;
