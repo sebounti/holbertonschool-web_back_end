0c7657833e8b24bb706970c5da8d2e091728b45a
lenne sebastien <sebastienlenne@gmail.com>
2024-03-25 10:34:59 +0100

    update file add typeof for arguments

--- unittests_in_js/0-calcul.js
+++ unittests_in_js/0-calcul.js
@@ -1,9 +1,11 @@
 // task 0-calcul.js
-
 function calculateNumber(a, b) {
+  if (typeof a !== "number" || typeof b !== "number") {
+    throw new TypeError("All arguments must be numbers");
+  }
+
   const rounded_a = Math.round(a);
   const rounded_b = Math.round(b);
-
   return rounded_a + rounded_b;
 }
 
