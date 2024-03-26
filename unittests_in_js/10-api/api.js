5bbe28a9041c3676380257aacfd0a16fb9b5b71b
lenne sebastien <sebastienlenne@gmail.com>
2024-03-26 13:19:27 +0100

    add new folder task 10

---
+++ unittests_in_js/10-api/api.js
@@ -0,0 +1,56 @@
+const express = require("express");
+
+const app = express();
+const port = 7865;
+
+// middleware
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.end("Welcome to the payment system");
+});
+
+app.get("/cart/:id(\\d+)", (req, res) => {
+  const id = req.params.id;
+  res.send(`Payment methods for cart ${id}`);
+});
+
+app.get("/available_payments", (req, res) => {
+  const payment_methods = {
+    payment_methods: {
+      credit_cards: true,
+      paypal: false,
+    },
+  };
+  res.json(payment_methods);
+});
+
+app.post("/login", (req, res) => {
+  const userName = req.body.userName;
+  if (userName) {
+    res.send(`Welcome ${userName}`);
+  } else {
+    res.status(400).send(`Username is required`);
+  }
+});
+
+// Gestion d'erreur "routes non trouvées"
+app.use((req, res) => {
+  res.status(404).send("Not Found");
+});
+
+// Gestion d'erreur "erreurs serveur"
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+// Démarrez le serveur
+app
+  .listen(port, () => {
+    console.log(`API available on localhost port ${port}`);
+    console.log(`Server listening at http://localhost:${port}`);
+  })
+  .on("error", (err) => {
+    console.error("Error starting server:", err);
+  });
---
+++ unittests_in_js/10-api/api.test.js
@@ -0,0 +1,86 @@
+const request = require("request");
+const { expect } = require("chai");
+const baseUrl = "http://localhost:7865";
+
+describe("Cart Endpoint", () => {
+  it("should return status code 200 and correct body when :id is a number", (done) => {
+    const id = 10;
+    request(`${baseUrl}/cart/${id}`, (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).equal(`Payment methods for cart 10`);
+      done();
+    });
+  });
+
+  it("should return status code 404 when :id is not a number", (done) => {
+    const cartId = "notanumber";
+    request(`${baseUrl}/cart/${cartId}`, (error, response, body) => {
+      expect(response.statusCode).to.equal(404);
+      done(); // Nécessaire pour les tests asynchrones
+    });
+  });
+
+  describe("testing integration", () => {
+    describe("GET", () => {
+      it("Code: 200 | Body: Welcome to the payment system", (done) => {
+        const options = {
+          url: "http://localhost:7865",
+          method: "GET",
+        };
+
+        request(options, function (error, response, body) {
+          expect(response.statusCode).to.equal(200);
+          expect(body).to.equal("Welcome to the payment system");
+          done();
+        });
+      });
+    });
+
+    describe("testing login", () => {
+      describe("POST /login", () => {
+        it("Responds 200 and correct name sebastien", (done) => {
+          const options = {
+            url: "http://localhost:7865/login",
+            method: "POST",
+            json: true,
+            body: {
+              userName: "sebastien",
+            },
+          };
+
+          request(options, function (error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            expect(body).to.equal("Welcome sebastien");
+            done();
+          });
+        });
+      });
+    });
+
+    describe("testing available payments", () => {
+      describe("POST /available_payments", () => {
+        it("Responds with 200 and correct JSON object when parsed", (done) => {
+          const options = {
+            url: "http://localhost:7865/available_payements",
+            method: "GET",
+          };
+
+          request(options, function (error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            const bodyParse = JSON.parse(body);
+
+            const bodyReference = {
+              payment_methods: {
+                credit_cards: true,
+                paypal: false,
+              },
+            };
+
+            expect(bodyParse).to.deep.equal(bodyReference);
+            done();
+          });
+        });
+      });
+    });
+  });
+});
